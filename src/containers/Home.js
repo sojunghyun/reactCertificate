0ea44ccee20e965b80d10080376be00eda0c3e5c
sojung.hyun <sojung.hyun@koreanclick.com>
2019-11-19 17:03:47 +0900

    랜덤수를 만들어 해당하는 index의 문제와 답을 보여준다.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,55 +13,51 @@ const list = [
   'num': 2,
   'problem': 'test 문제2',
   'answer': 'test 답2'
+  },
+  {
+  'num': 3,
+  'problem': 'test 문제3',
+  'answer': 'test 답3'
+  },
+  {
+  'num': 4,
+  'problem': 'test 문제4',
+  'answer': 'test 답4'
+  },
+  {
+  'num': 5,
+  'problem': 'test 문제5',
+  'answer': 'test 답5'
   }
 ]
-  
 
-const names = ['안녕하세요.', '방갑습니다.', '퇴근합시다.'];
-
-// const list = [
-//   {
-//   'num': 1,
-//   'problem': 'test 문제1',
-//   'answer': 'test 답1'
-//   },
-//   {
-//   'num': 2,
-//   'problem': 'test 문제2',
-//   'answer': 'test 답2'
-//   },
-//   {
-//   'num': 3,
-//   'problem': 'test 문제3',
-//   'answer': 'test 답3'
-//   }
-//   ]
 class Home extends Component {
 
+  // 상태(state)는 컴포넌트의 상태를 나타냄. 변화할 수 있는 데이터의 집합
   state = {
     number: 0
   }
 
+  //list에서 무엇을 가져올지 지정해준 후 객체를 넘겨준다. this.setState사용
   searchList(questionList, randNum){
     console.log(questionList[randNum-1]['problem']);
+    const index = questionList[randNum-1]['num'];
+    const problem = questionList[randNum-1]['problem'];
+    const answer = questionList[randNum-1]['answer'];
+    this.setState({ result_problem: problem, result_answer: answer, result_index: index });
   }
-
-  handleIncrease = () => {
-    const { number } = this.state;
-    this.setState({
-      number: number + 1
-    });
-  }
-
+  //프로퍼티(props) 외부에서 컴포넌트와 소통하는 창구 역할. 일반적으로 프로퍼티의 값은 부모 요소에서 설정. 
+  //또한 한 번 설정된 프로퍼티는 기본적으로 컴포넌트 내부에서 변경 X, 프로퍼티는 초깃값 설정과 자료형 유효성 검사 등을 사용할 수 있음
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { random: 1 };
+    this.onChangeClick = this.onChangeClick.bind(this);
+    // 초기 값 설정 가능
+    // this.state = { random: 1 };
   }
 
-  handleClick() {
+  onChangeClick() {
     const min = 1;
-    const max = 2;
+    const max = 5;
     const rand = Math.floor(Math.random()*(max+1-min))+min;
     this.setState({ random: rand });
     this.searchList(list, rand)
@@ -69,59 +65,19 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
- 
-        <button onClick={this.handleClick.bind(this)}>Click</button>
-        <div>The number is: {this.state.random}</div>
-                  <List 
-                  num={list.num}
-                  problem={list.problem}
-                  answer={list.answer}
-                  />
-        }
-
-      </div>
-
-    );
-  }
-    // render() {
-    //     return (
-    //         <div className="button-item">
-    //             <AuthContent title="로그인">
-    //             <Input label="이메일" name="email" placeholder="이메일"/>
-    //             <Input label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
-    //             </AuthContent>
-    //             <button onClick={this.LoggingButton}>submit</button>
-    //         </div>
-    //
-    //     );
-    // }
-
-}
-class LoggingButton extends React.Component {
-  handleClick() {
-    console.log('this is:', this);
-  }
-
-  render() {
-        return (
       <div className="button-item">
-        <div>login</div>
+        
+        <div>The number is: {this.state.random}</div>
+        <div>The Problem is: {this.state.result_problem}</div>
+        <div>The answer is: {this.state.result_answer}</div>
+        <div>The index is: {this.state.result_index}</div>
+
+        <button onClick={this.onChangeClick.bind(this)}>Click</button>
 
       </div>
+
     );
   }
 }
+
 export default Home;
-//  const Home = () => {
-//      return (
-//          <div>
-//              <BigText>Users</BigText>
-//              {/*{this.state.users.map(user =>*/}
-//              {/*    <div key={user._id}>{user.name} {user.age}</div>*/}
-//              )}
-//          </div>
-//      );
-//  };
-//
-// export default Home;
